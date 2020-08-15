b484c80919988708d434ce4b01fa1fb2b895270f
Theo Salzmann <theo.salzmann@sandstorm.de>
2020-08-15 09:58:32 +0200

    TASK: Basic implementation

---
+++ README.md
@@ -0,0 +1,12 @@
+## Data parser
+
+This is a simple data parser for [aer-data](https://github.com/on3iro/aer-data).
+
+To run the parser, follow these steps:
+
+1. check out the repository
+2. run `yarn` to install all dependencies
+3. (optional) check if a newer version of aer-data exists and install it via yarn
+4. run `yarn start`
+
+>*NOTE*: The script isn't very sophisticated so there might occur issues with file paths, directories etc.
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,53 @@
-const AERData = require('aer-data/dist/cjs/index.js')
+const fs = require('fs')
+const AERData = require("aer-data/dist/cjs/index.js");
+const OTCSV = require("objects-to-csv");
 
-const { normalizedData } = AERData.default
-console.log(normalizedData)
+const { normalizedData } = AERData.default;
+
+const BASE_PATH = "./data";
+
+const languageKeys = Object.keys(normalizedData);
+
+languageKeys.forEach(langKey => {
+  const language = normalizedData[langKey];
+  const {
+    expansions,
+    expansionIds,
+    nemeses,
+    nemesisIds,
+    mages,
+    mageIds,
+    cards,
+    cardIds,
+    treasures,
+    treasureIds,
+    basicNemesisCards,
+    basicNemesisCardIds,
+    upgradedBasicNemesisCards,
+    upgradedBasicNemesisCardIds,
+  } = language;
+
+  expansionIds.forEach((expansionId: any) => {
+    const expansion = expansions[expansionId]
+    const path = `${BASE_PATH}/${langKey}/${expansion.id}`;
+    fs.mkdir(path, { recursive: true }, (err: any) => {
+      if (err) throw err;
+    });
+
+    const nemesesByExpansion = nemesisIds.map((id: any) => nemeses[id]).filter((n: any) => n.expansion === expansion.id)
+    const magesByExpansion = mageIds.map((id: any) => mages[id]).filter((m: any) => m.expansion === expansion.id)
+    const cardsByExpansion = cardIds.map((id: any) => cards[id]).filter((c: any) => c.expansion === expansion.id)
+    const treasuresByExpansion = treasureIds.map((id: any) => treasures[id]).filter((t: any) => t.expansion === expansion.id)
+    const basicNemesisCardsByExpansion = basicNemesisCardIds.map((id: any) => basicNemesisCards[id]).filter((b: any) => b.expansion === expansion.id)
+    const upgradedBasicNemesisCardsByExpansion = upgradedBasicNemesisCardIds.map((id: any) => upgradedBasicNemesisCards[id]).filter((u: any) => u.expansion === expansion.id)
+
+    new OTCSV(nemesesByExpansion).toDisk(`${path}/nemeses.csv`);
+    new OTCSV(magesByExpansion).toDisk(`${path}/mages.csv`);
+    new OTCSV(cardsByExpansion).toDisk(`${path}/cards.csv`);
+    new OTCSV(treasuresByExpansion).toDisk(`${path}/treasures.csv`);
+    new OTCSV(basicNemesisCardsByExpansion).toDisk(`${path}/basicNemesisCards.csv`);
+    new OTCSV(upgradedBasicNemesisCardsByExpansion).toDisk(
+      `${path}/upgradedBasicNemesisCards.csv`
+    );
+  });
+});
