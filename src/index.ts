9070620f8d7578ec20511d12caa49bb2051ae843
Theo Salzmann <theo.salzmann@sandstorm.de>
2022-01-16 09:00:50 +0100

    Update to newest types and data

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-const fs = require('fs')
+const fs = require("fs");
 const AERData = require("aer-data/dist/cjs/index.js");
 const OTCSV = require("objects-to-csv");
 
@@ -8,7 +8,7 @@ const BASE_PATH = "./data";
 
 const languageKeys = Object.keys(normalizedData);
 
-languageKeys.forEach(langKey => {
+languageKeys.forEach((langKey) => {
   const language = normalizedData[langKey];
   const {
     expansions,
@@ -28,24 +28,39 @@ languageKeys.forEach(langKey => {
   } = language;
 
   expansionIds.forEach((expansionId: any) => {
-    const expansion = expansions[expansionId]
+    console.log({ expansionId, langKey });
+    const expansion = expansions[expansionId];
     const path = `${BASE_PATH}/${langKey}/${expansion.id}`;
     fs.mkdir(path, { recursive: true }, (err: any) => {
       if (err) throw err;
     });
 
-    const nemesesByExpansion = nemesisIds.map((id: any) => nemeses[id]).filter((n: any) => n.expansion === expansion.id)
-    const magesByExpansion = mageIds.map((id: any) => mages[id]).filter((m: any) => m.expansion === expansion.id)
-    const cardsByExpansion = cardIds.map((id: any) => cards[id]).filter((c: any) => c.expansion === expansion.id)
-    const treasuresByExpansion = treasureIds.map((id: any) => treasures[id]).filter((t: any) => t.expansion === expansion.id)
-    const basicNemesisCardsByExpansion = basicNemesisCardIds.map((id: any) => basicNemesisCards[id]).filter((b: any) => b.expansion === expansion.id)
-    const upgradedBasicNemesisCardsByExpansion = upgradedBasicNemesisCardIds.map((id: any) => upgradedBasicNemesisCards[id]).filter((u: any) => u.expansion === expansion.id)
+    const nemesesByExpansion = nemesisIds
+      .map((id: any) => nemeses[id])
+      .filter((n: any) => n.expansion === expansion.id);
+    const magesByExpansion = mageIds
+      .map((id: any) => mages[id])
+      .filter((m: any) => m.expansion === expansion.id);
+    const cardsByExpansion = cardIds
+      .map((id: any) => cards[id])
+      .filter((c: any) => c.expansion === expansion.id);
+    const treasuresByExpansion = treasureIds
+      .map((id: any) => treasures[id])
+      .filter((t: any) => t.expansion === expansion.id);
+    const basicNemesisCardsByExpansion = basicNemesisCardIds
+      .map((id: any) => basicNemesisCards[id])
+      .filter((b: any) => b.expansion === expansion.id);
+    const upgradedBasicNemesisCardsByExpansion = upgradedBasicNemesisCardIds
+      .map((id: any) => upgradedBasicNemesisCards[id])
+      .filter((u: any) => u.expansion === expansion.id);
 
     new OTCSV(nemesesByExpansion).toDisk(`${path}/nemeses.csv`);
     new OTCSV(magesByExpansion).toDisk(`${path}/mages.csv`);
     new OTCSV(cardsByExpansion).toDisk(`${path}/cards.csv`);
     new OTCSV(treasuresByExpansion).toDisk(`${path}/treasures.csv`);
-    new OTCSV(basicNemesisCardsByExpansion).toDisk(`${path}/basicNemesisCards.csv`);
+    new OTCSV(basicNemesisCardsByExpansion).toDisk(
+      `${path}/basicNemesisCards.csv`
+    );
     new OTCSV(upgradedBasicNemesisCardsByExpansion).toDisk(
       `${path}/upgradedBasicNemesisCards.csv`
     );
